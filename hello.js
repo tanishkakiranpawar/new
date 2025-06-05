aa72e693390a2210329406d4af9971985a336cbd
tanishkakiranpawar <tanishkapawar081207@gmail.com>
2025-06-05 13:29:32 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,15 @@
+console.log("hello world");
+let fil = 15;
+fil = 18;
+fil = 'this'
+console.log(fil);
+const bal = 3.14
+console.log(bal);
+
+let x=5;
+if(x%2==0)
+    console.log("Even number");
+else(x%2!=0)
+    console.log("odd number");
+
+    
